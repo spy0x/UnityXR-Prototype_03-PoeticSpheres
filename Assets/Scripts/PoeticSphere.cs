6086557bcefbf726ce5f64c9f5ba2722eb06f6fc
Francisco V. Diaz <fevarga1@uc.cl>
2025-01-06 12:39:57 -0300

    added orbs asset pack. Poetic Spheres with random orb spawn working

--- Assets/Scripts/PoeticSphere.cs
+++ Assets/Scripts/PoeticSphere.cs
@@ -5,9 +5,20 @@ using UnityEngine.Serialization;
 public class PoeticSphere : MonoBehaviour
 {
     [SerializeField] SnapInteractable snapZone;
+    [SerializeField] GameObject[] orbPrefabs;
+
+    private GameObject currentOrb;
 
     void Start()
     {
         if (snapZone) snapZone.transform.SetParent(null);
+        SpawnRandomOrb();
+    }
+
+    private void SpawnRandomOrb()
+    {
+        if (orbPrefabs.Length == 0) return;
+        var prefab = orbPrefabs[Random.Range(0, orbPrefabs.Length)];
+        currentOrb = Instantiate(prefab, transform.position, Quaternion.identity, transform);
     }
 }
